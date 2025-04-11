bfca2ba498f0a8f0d54c21b35b81936afee921a6
tammor006 <ali.tamoor.0086@gmail.com>
2025-04-12 02:21:22 +0500

    12april

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000", // Allow frontend
+    origin: "https://premiermarketing.vercel.app/", // Allow frontend
     credentials: true, // Allow cookies (if needed)
   }));
-  console.log("Mongo URI:", process.env.MONGO_URI);
-  console.log(process.env.CLOUDINARY_CLOUD_NAME)
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
